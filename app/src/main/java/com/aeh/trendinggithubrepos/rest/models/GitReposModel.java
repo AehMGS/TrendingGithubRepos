820b97192616f1b15daa389723ece954cc7010ac
AEH <aeh.elbaraka@gmail.com>
2019-04-12 14:19:03 +0100

    - Create Repos model

---
+++ app/src/main/java/com/aeh/trendinggithubrepos/rest/models/GitReposModel.java
@@ -0,0 +1,51 @@
+package com.aeh.trendinggithubrepos.rest.models;
+
+public class GitReposModel {
+
+    private String title;
+    private String description;
+    private String avatarUrl;
+    private String username;
+    private String rating;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
